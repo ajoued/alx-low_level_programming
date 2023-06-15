3aa0bae75edf281c724a88e25a4a05e0bf602746
ajoued <ajoueds@gmail.com>
2023-06-15 20:21:18 +0000

    random automated git commit message

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * prints exactely and that piece of art is useful\" - Dora Korpar, 2015-10-19
+ * Return: 0 Always
+ */
+int main(void)
+{
+	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int i;
+
+	while (msg[i] != '\0')
+	{
+		putchar(msg[i]);
+		i++;
+	}
+return (1);
+}
