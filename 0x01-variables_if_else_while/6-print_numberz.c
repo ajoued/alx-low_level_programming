391f48d35778e3d8dded946e2a85048a85204380
ajoued <ajoueds@gmail.com>
2023-06-16 14:57:40 +0000

    random automated git commit message

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * prints 0123456789 using only putchar
+ * Retur: 0 Always
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(48+i);
+		i++;
+	}
+	putchar('\n');
+return (0);
+}
