e8746ccfbd84734180c2c40f9c05049a8249610a
ajoued <ajoueds@gmail.com>
2023-06-16 15:09:05 +0000

    random automated git commit message

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -4,5 +4,18 @@
  * main - Entry point
  *
  * prints the alphabet in revrse
+ * Return: 0 Always
  */
+int main(void)
+{
+	char msg[27] = "zyxwvutsrqponmlkjihgfedcba";
+	int i = 0;
 
+	while (msg[i] != '\0')
+	{
+		putchar(msg[i]);
+		i++;
+	}
+	putchar('\n');
+return (0);
+}
