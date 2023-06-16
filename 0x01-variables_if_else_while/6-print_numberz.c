6edbbb53e490460d1db6a7bbc0dc771395f36f18
ajoued <ajoueds@gmail.com>
2023-06-16 15:00:34 +0000

    random automated git commit message

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -11,7 +11,7 @@ int main(void)
 
 	while (i < 10)
 	{
-		putchar(48+i);
+		putchar(48 + i);
 		i++;
 	}
 	putchar('\n');
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * prints the alphabet in revrse
+ */
+
