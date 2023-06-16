a742b78dbe2f5852e608260e2941b92b02374afd
ajoued <ajoueds@gmail.com>
2023-06-16 13:32:33 +0000

    random automated git commit message

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * print's the alphabet in lowercase
+ * return: 0 Always
+ */
+int main(void)
+{
+	char alphabet[27] ="abcdefghijklmnopqrstuvwsyz";
+	int i = 0;
+	while (alphabet[i] != '\0')
+	{
+		putchar(alphabet[i]);
+		i++;
+	}
+	putchar('\n');
+return (0);
+}
