81edc5c4f42852545b107123532886c269860b52
ajoued <ajoueds@gmail.com>
2023-06-16 15:14:08 +0000

    random automated git commit message

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * prints the alphabet in revrse
+ * Return: 0 Always
+ */
+int main(void)
+{
+	char msg[17] = "0123456789abcdef";
+	int i = 0;
+
+	while (msg[i] != '\0')
+	{
+		putchar(msg[i]);
+		i++;
+	}
+	putchar('\n');
+return (0);
+}
