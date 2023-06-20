aa116e71f0fc58cfff686fd96bc8113c8311b529
ajoued <ajoueds@gmail.com>
2023-06-20 18:21:49 +0100

    random automated git commit message

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,11 +8,12 @@
 void print_alphabet(void)
 {
 	int i = 0;
-	char alpha[27] = "abcdefghijklmnopqrstuvwxyz";		
+	char alpha[27] = "abcdefghijklmnopqrstuvwxyz";
+
 	while (alpha[i] != '\0')
-        {
-                _putchar(alpha[i]);
-                i++;
-        }
+	{
+	_putchar(alpha[i]);
+	i++;
+	}
 	_putchar('\n');
 }
