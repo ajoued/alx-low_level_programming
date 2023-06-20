dd42c4b49c9b90e0c5f7adc9510597c1df65c2af
ajoued <ajoueds@gmail.com>
2023-06-21 00:42:01 +0100

    random automated git commit message

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ *
+ * @n: the digit to be treated
+ *
+ * Return: the value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int a;
+
+	a = n % 10;
+return (_abs(a));
+}
