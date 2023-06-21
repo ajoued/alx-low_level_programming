56ef3fadeaec9cdb52a9e5e3f4bc67d4a53f38c9
ajoued <ajoueds@gmail.com>
2023-06-21 16:06:49 +0100

    random automated git commit message

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,6 +2,7 @@
 
 /**
 * main - causes an infinite loop
+* fixing infinite loop
 * Return: 0
 */
 
@@ -12,11 +13,11 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-/**
-*	while (i < 10)
-*	{
-*		putchar(i);
-*	}
+/*
+	while (i < 10)
+	{
+		putchar(i);
+	}
 */
 	printf("Infinite loop avoided! \\o/\n");
 
