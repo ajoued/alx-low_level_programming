904111c440f7aed0f7b3842c0c325f65c704afb0
ajoued <ajoueds@gmail.com>
2023-06-21 04:57:18 +0100

    random automated git commit message

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,7 +2,7 @@
 /**
  * main - prints the first 98 fibonacci numbers, starting with
  * 1 and 2, sepated by a comma followed by a space.
- * Return: 0 Always 
+ * Return: 0 Always
  */
 int main(void)
 {
