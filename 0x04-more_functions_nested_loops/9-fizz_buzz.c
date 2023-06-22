45067f9c247d9f0915fbc962712905973ec9238c
ajoued <ajoueds@gmail.com>
2023-06-22 17:03:34 +0100

    random automated git commit message

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,8 +17,10 @@ int main(void)
 				printf("FizzBuzz ");
 			else if (i % 3 == 0)
 				printf("Fizz ");
-			else
+			else if (i != 100)
 				printf("Buzz ");
+			else
+				printf("Buzz");
 		}
 		else
 			printf("%d ", i);
