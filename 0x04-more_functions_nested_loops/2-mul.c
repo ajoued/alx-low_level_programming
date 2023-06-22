58705811628e995d5291960b3d18a626f9dbfde8
ajoued <ajoueds@gmail.com>
2023-06-22 11:53:17 +0100

    random automated git commit message

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: first operand
+ * @b: second operand
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	int r;
+
+	r = a * b;
+	return (r);
+}
