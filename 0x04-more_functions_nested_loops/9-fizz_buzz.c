6cca788d61c0c7e7c32dcfe3fc6ea3756350648a
ajoued <ajoueds@gmail.com>
2023-06-22 16:47:16 +0100

    random automated git commit message

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * return: 0 always
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			if (i % 3 == 0 && i % 5 == 0)
+				printf("FizzBuzz ");
+			else if (i % 3 == 0)
+				printf("Fizz ");
+			else
+				printf("Buzz ");
+		}
+		else
+			printf("%d ", i);
+	}
+	return (0);
+}
