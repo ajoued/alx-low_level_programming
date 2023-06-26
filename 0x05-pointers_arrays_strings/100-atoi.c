0f34c1c109653c65eef218b80901f8e34d5ca4fa
ajoued <ajoueds@gmail.com>
2023-06-26 22:11:26 +0100

    random automated git commit message

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: the string ro be converted
+ * Return: the integer
+ */
+
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
