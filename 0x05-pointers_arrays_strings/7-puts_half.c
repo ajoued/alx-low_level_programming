ffccaf94f4bf622730fe82695c8adb891640127d
ajoued <ajoueds@gmail.com>
2023-06-26 19:17:02 +0100

    random automated git commit message

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - prints half of a string
+ * @str: string to be treated
+ * Return: nothing
+ */
+
+void puts_half(char *str)
+{
+	int length = strlen(str);
+	int i;
+
+	if (length % 2 == 0)
+	{
+		i = length / 2;
+		while (i <= length - 1)
+		{
+			_putchar(str[i]);
+			i++;
+		}
+	}
+	else
+	{
+		i = (length - 1) / 2;
+		while (i <= length - 1)
+		{
+			_putchar(str[i]);
+			i++;
+		}
+	}
+	_putchar('\n');
+}
