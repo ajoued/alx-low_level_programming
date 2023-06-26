97f180989b03f52ed8bff254e8a471ce14f8a323
ajoued <ajoueds@gmail.com>
2023-06-26 18:21:47 +0100

    random automated git commit message

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers.
+ * @a: the first integer
+ * @b: the second integer
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
