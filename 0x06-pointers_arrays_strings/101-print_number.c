6413ccb86232f6e41a58a671dc7bf377498321bd
ajoued <ajoueds@gmail.com>
2023-06-29 22:50:47 +0100

    random automated git commit message

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,28 +8,30 @@
 
 void print_number(int n)
 {
-	int t = n;
+	unsigned int t;
 	int divisor = 1;
 
 	if (n < 0)
 	{
+		n = n * -1;
 		_putchar('-');
-		n = -n;
 	}
 	if (n == 0)
-		_putchar('0');
-	else
 	{
-		while (t / 10 != 0)
-		{
-			divisor = divisor * 10;
-			t = t / 10;
-		}
-		_putchar('0' + n / divisor);
-		while (divisor != 1)
-		{
-			divisor = divisor / 10;
-			_putchar('0' + (n / divisor) % 10);
-		}
+		_putchar('0');
+		return;
 	}
+	t = n;
+	while (t / 10 != 0)
+	{
+		divisor = divisor * 10;
+		t = t / 10;
+	}
+	_putchar('0' + n / divisor);
+	while (divisor > 10)
+	{
+		divisor = divisor / 10;
+		_putchar('0' + (n / divisor) % 10);
+	}
+	_putchar('0' + n % 10);
 }
