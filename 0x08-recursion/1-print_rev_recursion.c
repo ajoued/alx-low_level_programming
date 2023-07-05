a1f6a06620768d5301eb832634d09fade1a1dcf9
ajoued <ajoueds@gmail.com>
2023-07-05 17:04:23 +0000

    random automated git commit message

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,12 +8,11 @@
 
 void _print_rev_recursion(char *s)
 {
+	if (s[0] == '\0')
+		return;
 	if (s[1] != '\0')
 	{
 		_print_rev_recursion(s + 1);
 	}
-	if (s[0] == '\0')
-		return;
-	else
-		_putchar(s[0]);
+	_putchar(s[0]);
 }
