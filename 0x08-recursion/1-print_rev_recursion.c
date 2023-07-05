474c0255992c74b8a5de5e5abcd2dd2e3ab86f4a
ajoued <ajoueds@gmail.com>
2023-07-05 16:50:55 +0000

    random automated git commit message

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string to be treated
+ * Return: nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (s[1] != '\0')
+	{
+		_print_rev_recursion(s + 1);
+	}
+	_putchar(s[0]);
+}
