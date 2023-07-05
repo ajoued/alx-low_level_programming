6dd028b00a1d82ddd30ddadbaca1b8f0bf30638c
ajoued <ajoueds@gmail.com>
2023-07-05 23:59:44 +0000

    random automated git commit message

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the integer to be treated
+ * Return: result
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (sqrt(n));
+}
