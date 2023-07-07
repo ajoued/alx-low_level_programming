cc5f2ccb12981a163e6c8024495197a48d1baf7e
ajoued <ajoueds@gmail.com>
2023-07-07 21:11:14 +0000

    random automated git commit message

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: arguments count
+ * @argv: array of arrguments
+ * Return: 0 Always success
+ */
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+this directory contains the solution of the alx "0x0A .C - argc, argv" project tasks
