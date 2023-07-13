92ec22ac96369efee5ca1e52bbfc42dbfe6a77cf
ajoued <ajoueds@gmail.com>
2023-07-13 17:03:56 +0000

    random automated git commit message

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: first string to be treated
+ * @s2: second string to be treated
+ * @n: number of bytes to be copied
+ * Return: pointer shall point to a newly allocated space in memory
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ptr;
+	unsigned int len, i, j;
+
+	len = strlen(s1);
+	ptr = (char *)malloc((len + n + 1) * sizeof(char));
+	for (i = 0; i < len; i++)
+	{
+		ptr[i] = s1[i];
+	}
+	j = len;
+	for (i = 0; i <= n; i++)
+	{
+		ptr[j] = s2[i];
+		j++;
+	}
+	ptr[j] = '\0';
+	return (ptr);
+}
