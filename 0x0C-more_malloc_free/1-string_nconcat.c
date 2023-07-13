fe3c15b499a561d776d27f96db81bdc23b25a2de
ajoued <ajoueds@gmail.com>
2023-07-13 17:07:50 +0000

    random automated git commit message

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,7 +15,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len, i, j;
 
 	len = strlen(s1);
-	ptr = (char *)malloc((len + n + 1) * sizeof(char));
+	ptr = (char *)malloc((len + n) * sizeof(char));
 	for (i = 0; i < len; i++)
 	{
 		ptr[i] = s1[i];
@@ -26,6 +26,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		ptr[j] = s2[i];
 		j++;
 	}
-	ptr[j] = '\0';
 	return (ptr);
 }
