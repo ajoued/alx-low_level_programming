839927d4d6b98eda0bddbfe4788ebb4ace834e55
ajoued <ajoueds@gmail.com>
2023-07-13 17:29:03 +0000

    random automated git commit message

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,25 +11,36 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ptr;
-	unsigned int len, i, j;
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	len = strlen(s1);
-	ptr = (char *)malloc((len + n + 1) * sizeof(char));
-	if (ptr == NULL)
-		exit(0);
-	for (i = 0; i < len; i++)
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (!s)
+		return (NULL);
+
+	while (i < len1)
 	{
-		ptr[i] = s1[i];
+		s[i] = s1[i];
+		i++;
 	}
-	j = len;
-	for (i = 0; i < n; i++)
-	{
-		if (s2[i] == '\0')
-			break;
-		ptr[j] = s2[i];
-		j++;
-	}
-	ptr[j] = '\0';
-	return (ptr);
+
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+
+	s[i] = '\0';
+
+	return (s);
 }
+
