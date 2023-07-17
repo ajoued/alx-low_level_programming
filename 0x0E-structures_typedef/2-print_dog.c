07f459a48debb9fca3e1cbc8614f5d0e6735622d
ajoued <ajoueds@gmail.com>
2023-07-17 21:04:19 +0100

    random automated git commit message

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * void print_dog - prints a struct dog
+ * @d: struct to be treated
+ * Return: nothing
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		else
+			printf ("Name: %s\n", d->name);
+		if (d->age == 0)
+			printf("Age: (nil)\n");
+		else
+			printf ("Age: %.1f\n", d->age);
+		if (d->owner == NULL)
+			printf ("Owner: (nil)\n");
+		else
+			printf ("Owner: %s\n", d->owner);
+	}
+}
