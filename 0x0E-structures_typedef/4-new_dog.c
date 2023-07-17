650f94138d4fc2f91a49fd0d9cd064ee2e717acc
ajoued <ajoueds@gmail.com>
2023-07-17 21:30:57 +0100

    random automated git commit message

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
+	return (d);
+}
