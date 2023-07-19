7f1cee08a79a6d60e88293e4cd2ced2e8cedbe09
ajoued <ajoueds@gmail.com>
2023-07-20 00:44:24 +0100

    random automated git commit message

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,13 +18,7 @@ int main(int argc, char **argv)
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 
-	if (strcmp(argv[2], "/") == 0 && b == 0)
-	{
-		printf("Error\n");
-		return (98);
-	}
-
-	if (strcmp(argv[2], "%") == 0 && b == 0)
+	if (argc != 4)
 	{
 		printf("Error\n");
 		return (98);
@@ -34,6 +28,12 @@ int main(int argc, char **argv)
 	{
 		printf("Error\n");
 		return (99);
+	}
+
+	if ((strcmp(argv[2], "/") == 0 && b == 0) || (strcmp(argv[2], "%") == 0 && b == 0))
+	{
+		printf("Error\n");
+		return (100);
 	}
 
 	printf("%d\n", foo(a, b));
