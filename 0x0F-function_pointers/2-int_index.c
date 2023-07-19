3149f571ea3c4c896433adc30301fedf3e20dc0c
ajoued <ajoueds@gmail.com>
2023-07-19 21:08:47 +0100

    random automated git commit message

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,10 +14,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
-	if (array == NULL || cmp == NULL)
-		return(0);
 
 	for (i = 0; i < size; i++)
 	{
