b5b84a01892b8386dc1ada03113d3acc54f34282
ajoued <ajoueds@gmail.com>
2023-07-20 01:54:32 +0100

    random automated git commit message

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -11,21 +11,24 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
-	int num_bytes = atoi(argv[1]);
-	char *code = (char *)main;
+	int i, num_bytes;
+	char *code;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return (1);
+		exit(1);
 	}
 
-	if (num_bytes <= 0)
+	num_bytes = atoi(argv[1]);
+
+	if (num_bytes < 0)
 	{
 		printf("Error\n");
-		return (2);
+		exit(2);
 	}
+
+	code = (char*)main;
 
 	for (i = 0; i < num_bytes; i++)
 	{
