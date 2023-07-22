4bc387ef6cc80e72b2140f3a4b89bef45f6ec5bf
ajoued <ajoueds@gmail.com>
2023-07-22 18:51:54 +0100

    random automated git commit message

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,11 +16,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(numbers, n);
 
-	for (i = 0; i < (int)n - 1; i++)
+	if (n != 0)
 	{
-		printf("%d%s", va_arg(numbers, int), separator);
+		for (i = 0; i < (int)n - 1; i++)
+		{
+			if (separator == NULL)
+				printf("%d", va_arg(numbers, int));
+			else
+			printf("%d%s", va_arg(numbers, int), separator);
+		}
+		printf("%d\n", va_arg(numbers, int));
 	}
-	printf("%d\n", va_arg(numbers, int));
-
 	va_end(numbers);
 }
