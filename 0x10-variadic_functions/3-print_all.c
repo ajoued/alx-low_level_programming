eebaf70a643d4f0c0eb2d69efe493c50584931c1
ajoued <ajoueds@gmail.com>
2023-07-23 14:36:31 +0100

    random automated git commit message

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,7 +10,7 @@
 void print_all(const char * const format, ...)
 {
 	int i = 0, len;
-	char *str;
+	char *str, *sep = "";
 	va_list list;
 
 	va_start(list, format);
@@ -20,51 +20,29 @@ void print_all(const char * const format, ...)
 		switch (format[i])
 		{
 			case 'c':
-				printf("%c, ", (char)va_arg(list, int));
+				printf("%s%c", sep, (char)va_arg(list, int));
 				break;
 			case 'i':
-				printf("%d, ", va_arg(list, int));
+				printf("%s%d", sep, va_arg(list, int));
 				break;
 			case 'f':
-				printf("%f, ", (float)va_arg(list, double));
+				printf("%s%f", sep, (float)va_arg(list, double));
 				break;
 			case 's':
 				str = va_arg(list, char *);
 				if (str == NULL)
 				{
-					printf("(nil), ");
+					printf("%s(nil)", sep);
 					break;
 				}
-				printf("%s, ", str);
+				printf("%s%s", sep, str);
 				break;
 			default:
 				break;
 		}
+		sep = ", ";
 		i++;
 	}
-	switch (format[i])
-                {
-                        case 'c':
-                                printf("%c", (char)va_arg(list, int));
-                                break;
-                        case 'i':
-                                printf("%d", va_arg(list, int));
-                                break;
-                        case 'f':
-                                printf("%f", (float)va_arg(list, double));
-                                break;
-                        case 's':
-                                str = va_arg(list, char *);
-                                if (str == NULL)
-                                {
-                                        printf("(nil)");
-                                        break;
-                                }
-                                printf("%s", str);
-                                break;
-                        default:
-                                break;
-                }
 
 	va_end(list);
 	putchar('\n');
