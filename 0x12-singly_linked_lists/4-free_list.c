0f2acd8c64f6b646e426906376681cd150d58d9e
ajoued <ajoueds@gmail.com>
2023-07-28 00:02:24 +0100

    random automated git commit message

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ * @head: head of the list to be treated
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *ptr, *temp;
+
+	ptr = head;
+	while (ptr != NULL)
+	{
+		temp = ptr;
+		ptr = ptr->next;
+		free(temp);
+	}
+	free(head);
+}
