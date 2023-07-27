d3d2321e7a50fa9582e834a26e2a1f74adb78691
ajoued <ajoueds@gmail.com>
2023-07-27 22:06:28 +0100

    random automated git commit message

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a node at the beginning of a list
+ * @head: pointer to the head of the list
+ * @str: string to be copied as value
+ * Return: the address of of the new element
+ * NULL if failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	unsigned int len;
+	list_t *temp = NULL;
+
+	temp = malloc(sizeof(list_t));
+	temp->next = *head;
+	temp->str = strdup(str);
+	len = strlen(str);
+	temp->len = len;
+	*head = temp;
+return (*head);
+}
