6742b0c9bc98297e8ad7d5178f77a6bed134f22b
ajoued <ajoueds@gmail.com>
2023-07-31 16:13:00 +0100

    random automated git commit message

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,4 +1,18 @@
 #include "lists.h"
 
 /**
- * free_listint
+ * free_listint - frees a list
+ * @head: head of the list to be treated
+ * Return: nothing
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp = NULL;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
