8541c461a4507c67eb969d9f6a80922ae6fdc292
ajoued <ajoueds@gmail.com>
2023-07-31 18:31:47 +0100

    random automated git commit message

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,8 +9,9 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp = NULL;
 
-	if (head == NULL)
-		exit(0);
+	if (*head == NULL)
+		return;
+
 	while (*head != NULL)
 	{
 		temp = (*head)->next;
