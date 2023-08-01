605eb6c57dd672e29b09371b191423a7451139ca
ajoued <ajoueds@gmail.com>
2023-08-01 18:55:03 +0100

    random automated git commit message

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,7 +11,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int i = 0;
 	listint_t *temp = NULL, *ptr = NULL;
 
-	if (i == 0)
+	if (index == 0)
 	{
 		temp = *head;
 		if (temp == NULL)
