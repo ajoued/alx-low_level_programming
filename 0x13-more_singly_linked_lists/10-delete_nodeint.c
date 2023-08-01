e948519c681f89b7991c8ebe244e7316ffa81384
ajoued <ajoueds@gmail.com>
2023-08-01 18:42:38 +0100

    random automated git commit message

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes a node at index in a list
+ * @head: head of the list to be treated
+ * @index: index of the node to be deleted
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *temp = NULL, *ptr = NULL;
+
+	if (i == 0)
+	{
+		temp = *head;
+		if (temp == NULL)
+			return (-1);
+		*head = temp->next;
+		free(temp);
+		return (1);
+	}
+	ptr = *head;
+	while (i < index - 1)
+	{
+		if (ptr->next == NULL)
+			return (-1);
+		ptr = ptr->next;
+		i++;
+	}
+	temp = ptr->next;
+	if (temp == NULL)
+		return (-1);
+	ptr->next = temp->next;
+	free(temp);
+	return (1);
+}
