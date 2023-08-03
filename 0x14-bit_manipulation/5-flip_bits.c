4599a3e194140de0e695c16a4755963913ab87ff
ajoued <ajoueds@gmail.com>
2023-08-04 00:43:08 +0100

    i think i got the password

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the number of bits you would need
+ * to flip to get from one number to another
+ * @n: first number
+ * @m: second number
+ * Return: the number needed
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int diff;
+
+	diff = n ^ m;
+
