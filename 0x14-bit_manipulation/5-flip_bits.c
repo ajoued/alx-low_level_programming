f608dba6c65381293d886c9343462808fb53f216
ajoued <ajoueds@gmail.com>
2023-08-04 01:06:05 +0100

    I think i got this

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,6 +10,13 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int diff;
+	unsigned int count = 0;
 
 	diff = n ^ m;
-
+	while (diff)
+	{
+		count = count + (diff & 1);
+		diff = diff >> 1;
+	}
+	return (count);
+}
