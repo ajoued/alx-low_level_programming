a2c020024eaf225cfe931ba412b3253570d2239f
ajoued <ajoueds@gmail.com>
2023-08-07 22:38:53 +0100

    it's working perfectly on my machine i hope the checker accept it without any errors

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of the file to be created
+ * @text_content: content of the file
+ * Return: 1 on success -1 in failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len = 0, written;
+
+	if (filename == NULL)
+		return (-1);
+	if (access(filename, F_OK) != 0)
+	{
+		fd = creat(filename, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	}
+	if (text_content != NULL)
+		len = strlen(text_content);
+
+	fd = open(filename, O_RDWR);
+	written = write(fd, text_content, len);
+
+	if (fd < 0 || written < 0)
+		return (-1);
+	close(fd);
+	return (1);
+}
