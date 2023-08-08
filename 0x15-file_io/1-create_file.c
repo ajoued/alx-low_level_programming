852911d320715d43e7680a430650efa972f50fad
ajoued <ajoueds@gmail.com>
2023-08-08 14:22:23 +0100

    ntohing

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content == NULL)
 	{
-		fd = open(filename, O_RDWR);
+		fd = open(filename, O_RDONLY);
 		if (fd != -1)
 			return (1);
 		return (-1);
