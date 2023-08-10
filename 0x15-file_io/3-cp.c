979651c9284cee5dfb8a53a57c032b128885cf3d
ajoued <ajoueds@gmail.com>
2023-08-10 16:52:12 +0100

    edits on task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,14 +2,50 @@
 #include <stdio.h>
 
 /**
+ * str_alloc - allocates 1024 for the string
+ * @file: file name if error occured
+ * Return: pointer to the allocated memory
+ */
+char *str_alloc(char *file)
+{
+	char *ptr;
+
+	ptr = malloc(sizeof(char) * 1024);
+	if (ptr == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+		exit(99);
+	}
+	return (ptr);
+}
+
+/**
+ * file_close - closes a file and
+ * @fd: file to be closed
+ * Return: 0 if success -1 if fails
+ */
+int file_close(int fd)
+{
+	int c;
+
+	c = close(fd);
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+	return (0);
+}
+
+/**
  * main - copies the content of a file to another file
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: 0 always success
+ * Return: 0 if success otherwise if it fails
  */
 int main(int argc, char **argv)
 {
-	int fd1, fd2, read1, write2, len = 0, c1, c2;
+	int src_fd, dest_fd, read1, write2;
 	char *file1, *file2, *str;
 
 	if (argc != 3)
@@ -19,36 +55,31 @@ int main(int argc, char **argv)
 	}
 	file1 = argv[1];
 	file2 = argv[2];
-	fd1 = open(file1, O_RDONLY);
-	len = lseek(fd1, 0, SEEK_END);
-	str = malloc(sizeof(char) * len);
-	lseek(fd1, 0, SEEK_SET);
-	read1 = read(fd1, str, len);
-	if (fd1 == -1 || read1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file1);
-		exit(98);
-	}
-	fd2 = open(file2, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	write2 = write(fd2, str, len);
-	if (fd2 == -1 || write2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file2);
-		exit(99);
-	}
+	str = str_alloc(file2);
+	src_fd = open(file1, O_RDONLY);
+	read1 = read(src_fd, str, 1024);
+	dest_fd = open(file2, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (src_fd == -1 || read1 == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file1);
+			free(str);
+			exit(98);
+		}
+		write2 = write(dest_fd, str, read1);
+		if (dest_fd == -1 || write2 == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file2);
+			free(str);
+			exit(99);
+		}
+		read1 = read(src_fd, str, 1024);
+		dest_fd = open(file2, O_WRONLY | O_APPEND);
+	} while (read1 > 0);
 	free(str);
-	c1 = close(fd1);
-	c2 = close(fd2);
-	if (c1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd1);
-		exit(100);
-	}
-	if (c2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd2);
-		exit(100);
-	}
+	file_close(src_fd);
+	file_close(dest_fd);
 
 	return (0);
 }
