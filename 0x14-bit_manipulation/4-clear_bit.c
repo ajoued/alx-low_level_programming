fa039559408d45194905746a99eab2af9f3efbb6
ajoued <ajoueds@gmail.com>
2023-08-31 21:51:28 +0100

    I got the result as expected on my machine hope this works

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets the value of a bit to 1 at a given index
+ * @n: number to be treated
+ * @index: index of the bit to be set to 1
+ * Return: 1 if it worked -1 if error
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int j = 1UL;
+
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	j = j << index;
+	j = ~j;
+	*n = *n & j;
+	if (!(*n & j))
+		return (1);
+	else
+		return (-1);
+}
