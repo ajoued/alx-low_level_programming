5250c92693f7b2cbc8303a46b0cc07656b691f79
ajoued <ajoueds@gmail.com>
2023-08-31 17:02:18 +0100

    Finished task 2 it is working in my machine time to test it

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number to be treated
+ * Return: nothing
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int j = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	int i = 0, first1 = 0, size;
+
+	size = sizeof(unsigned long int) * 8;
+	if (n == 0)
+		_putchar('0');
+	else
+		while (i <= size)
+		{
+			if (n & j)
+			{
+				first1 = 1;
+				_putchar('1');
+			}
+			else if (first1 == 1)
+			{
+				_putchar('0');
+			}
+			j = j >> 1;
+			i++;
+		}
+}
